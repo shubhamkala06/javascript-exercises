035104f0036ff03e62d77f7c6faaaed7bef9ed56
shubhamkala06 <25shubhamkala@gmail.com>
2023-01-23 13:26:31 +0530

    first few exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, n) {
+    let value = "";
+    for(let i=0;i<n;i++){
+        value += word;
+    }
+    return(value);
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let res = "";
+    for (let index = string.length-1; index >= 0; index--){
+        res += string[index];
+    } 
+    return res;
 };
 
 // Do not edit below this line
