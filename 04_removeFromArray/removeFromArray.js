bd96d475909fe15f77fda63046c39f4907dad6cb
shubhamkala06 <25shubhamkala@gmail.com>
2023-01-24 23:55:12 +0530

    exercises 4,5,6

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function(PArray) {
-
+const removeFromArray = function(array, ...rm) {
+    let result = array.filter((item)=>{
+        if(!(rm.includes(item)))
+        {
+            return item;
+        }
+    })
+    return result;
 };
+
+// Rest parameters, Array.filter(), Array.includes()
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    if((typeof n1 != typeof 1) || (typeof n2 != typeof 1) || (n1*n2 < 0))
+    {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    let init = 0, limit = 0;
+    if(n1<n2){
+        init = n1;
+        limit = n2;
+    }
+    else{
+        init = n2;
+        limit = n1;
+    }
+    for(;init<=limit;init++)
+    {
+        sum += init;
+    }
+
+    return sum;
 
 };
 
