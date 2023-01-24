aee7a7bcb945a93569fe53f060cf093f2cdeeaa3
shubhamkala06 <25shubhamkala@gmail.com>
2023-01-25 02:24:49 +0530

    7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 1);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToCelsius = function(temp) {
+  result = round((temp - 32)*(5/9));
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  result = round((temp*(9/5))+32);
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
