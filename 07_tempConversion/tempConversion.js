379e88d49172af2c3e87191fd480e505b27b907d
shubhamkala06 <25shubhamkala@gmail.com>
2023-01-26 15:08:02 +0530

    Fundamental exercises complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,4 @@
-function round(value, precision) {
+function round(value, precision) {            //taken from internet
   var multiplier = Math.pow(10, precision || 1);
   return Math.round(value * multiplier) / multiplier;
 }
@@ -12,6 +12,10 @@ const convertToFahrenheit = function(temp) {
   result = round((temp*(9/5))+32);
   return result;
 };
+
+
+
+//Don't know that even without exporting round function, other files were able to use round inside converToCelsius and convertToFarenheit
 
 // Do not edit below this line
 module.exports = {
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  return array.reduce((accumulater,currentvalue)=>{return accumulater +currentvalue},sum);      //taken from internet
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  return array.reduce((accumulater,currentvalue)=>{return accumulater*currentvalue},product);     //taken from internet
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1;
+  for(;num>1;num--){
+    factorial *= num;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
