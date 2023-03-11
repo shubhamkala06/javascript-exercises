9cf8c64c6708befaccca90442e84404935d5ab0f
shubhamkala06 <25shubhamkala@gmail.com>
2023-03-11 09:07:14 +0530

    palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const reverse = require('../03_reverseString/reverseString.js');
+const palindromes = function (string) {
+    string=string.toLowerCase();
+    string = string.trim();
+    let arr = string.split('');
+    arr = arr.filter((item)=>{if(item<='z'&&item>='a'){
+        return item;
+    }})
+    string = arr.toString().replaceAll(',','');
+    if(string == reverse(string)){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
-
+//ZZZZ car, a man, a maracaz.
 // Do not edit below this line
 module.exports = palindromes;
