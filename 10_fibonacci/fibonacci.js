ccee6ea57be38bd318ff72ebeabe5834daf37b8e
shubhamkala06 <25shubhamkala@gmail.com>
2023-03-11 10:21:57 +0530

    exercise 11 and 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = +n;
+    if(n<1){
+        return "OOPS";
+    }
+    let n1 = 1,n2 = 1;
+    if(n == 1||n == 2){
+       return 1;
+    }
+    else{
+        for(let i=0;i<(n-2);i++){
+            let temp = n2;
+            n2 = n1 + n2;
+            n1 = temp;
+        }
+        return n2;
+    }
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(Books) {
+    let titles = [];
+    for (let i of Books) {
+        titles.push(i['title']);
+    }
+    return titles;
 };
 
 // Do not edit below this line
