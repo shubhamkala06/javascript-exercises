6066dc8e384cbdccb95fdab52331434d113c7bae
shubhamkala06 <25shubhamkala@gmail.com>
2023-03-12 13:36:40 +0530

    exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let max_Age = 0;
+    let presentYear = new Date();
+    presentYear=presentYear.getFullYear();
+    let obj = {};
+    for(const i of arr){
+        if(i.yearOfDeath == undefined){
+            let age = presentYear - i.yearOfBirth;
+            if(age>max_Age){
+                max_Age = age;
+                obj = i;
+            }
+        }
+        else{
+            let age = i.yearOfDeath - i.yearOfBirth;
+            if(age>max_Age){
+                max_Age = age;
+                obj = i;
+            }
+        }
+    }
+    return obj;
 };
 
 // Do not edit below this line
